ea7593cdbe14e8d6f2d21dd005fa69c1dce50b86
Andrii Khomenko <khomenkoam007@gmail.com>
2020-07-21 23:53:48 +0300

    test commit

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,8 +4,10 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
+    this.store = options.store
     this.emitter = options.emitter
     this.unsubscribers = []
+    this.storeSub = null
 
     this.prepare()
   }
@@ -29,6 +31,14 @@ export class ExcelComponent extends DomListener {
     this.unsubscribers.push(unsub)
   }
 
+  $dispatch(action) {
+    this.store.dispatch(action)
+  }
+
+  $subscribe(fn) {
+    this.storeSub = this.store.subscribe(fn)
+  }
+
   // Инициализируем компонент
   // Добавляем DOM слушателей
   init() {
@@ -40,5 +50,6 @@ export class ExcelComponent extends DomListener {
   destroy() {
     this.removeDOMListeners()
     this.unsubscribers.forEach(unsub => unsub())
+    this.storeSub.unsubscribe()
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import {Header} from '@/components/header/Header';
 import {Toolbar} from '@/components/toolbar/Toolbar';
 import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
+import {rootReducer} from '@/redux/rootReducer';
+import {createStore} from '@core/createStore';
+
+const store = createStore(rootReducer, {
+  tableTitle: 'My table!'
+})
 
 const excel = new Excel('#app', {
-  components: [Header, Toolbar, Formula, Table]
+  components: [Header, Toolbar, Formula, Table],
+  store
 })
 
 excel.render()
